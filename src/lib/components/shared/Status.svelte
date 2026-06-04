0a9d858e6bc4acca669b687d63a38067c4517dbd
kevinkeyssx <keyss1700@gmail.com>
2026-06-03 22:51:08 -0400

    Status added

---
+++ src/lib/components/shared/Status.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	interface Props {
+		status      : boolean;
+        textTrue?   : string;
+        textFalse?  : string;
+	}
+
+	let {
+        status,
+        textTrue    = 'Activo',
+        textFalse   = 'Inactivo'
+    } : Props = $props();
+</script>
+
+{#if status }
+	<span class="rounded-full bg-emerald-500/10 px-2 py-0.5 text-xs text-emerald-400 font-bold border border-emerald-500/20">
+        { textTrue }
+    </span>
+{:else}
+	<span class="rounded-full bg-red-500/10 px-2 py-0.5 text-xs text-red-400 font-bold border border-red-500/20">
+        { textFalse }
+    </span>
+{/if}
