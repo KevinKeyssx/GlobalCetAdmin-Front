41ee1c830cc8f10c50f80142274eae5c2fceddd5
kevinkeyssx <keyss1700@gmail.com>
2026-06-03 23:00:29 -0400

    PageServerLoad deleted

--- src/routes/infrastructure/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { INFRASTRUCTURE_VIDEO_URL } from '$env/static/private';
-import type { PageServerLoad }       from './$types';
-
-export const load: PageServerLoad = async ( ) => {
-	return {
-		videoUrl : INFRASTRUCTURE_VIDEO_URL,
-	};
-};
