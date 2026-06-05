145a24f3d801c192e70a70b47b1e8041f178aa1f
kevinkeyssx <keyss1700@gmail.com>
2026-06-04 21:46:44 -0400

    FormData validates implemented

--- src/lib/services/fetch.service.ts
+++ src/lib/services/fetch.service.ts
@@ -6,7 +6,7 @@ import { PUBLIC_BACKEND_URL } from '$env/static/public';
 type Connect = {
 	endpoint    : string;
 	method?     : METHOD;
-	body?       : object;
+	body?       : object | FormData;
 	isInternal? : boolean;
 	headers?    : Record<string, string>;
 	fetch?      : typeof fetch;
@@ -33,22 +33,23 @@ export default async function connectRequest<T>( {
 	isInternal = true,
 	fetch: customFetch = fetch,
 }: Connect ): Promise<T | ApiError> {
-	const baseUrl   = isInternal ? '/' : PUBLIC_BACKEND_URL;
-	const path      = isInternal ? `api/${endpoint}` : endpoint;
-	const cleanBase = baseUrl.replace( /\/+$/, '' );
-	const cleanPath = path.replace( /^\/+/, '' );
-	const finalURL  = isInternal ? `/${cleanPath}` : `${cleanBase}/${cleanPath}`;
+	const baseUrl       = isInternal ? '/' : PUBLIC_BACKEND_URL;
+	const path          = isInternal ? `api/${endpoint}` : endpoint;
+	const cleanBase     = baseUrl.replace( /\/+$/, '' );
+	const cleanPath     = path.replace( /^\/+/, '' );
+	const finalURL      = isInternal ? `/${cleanPath}` : `${cleanBase}/${cleanPath}`;
+	const isFormData    = body instanceof FormData;
 
 	const response = await customFetch( finalURL, {
 		method,
-		body    : body ? JSON.stringify( body ) : undefined,
+		body    : isFormData ? body : ( body ? JSON.stringify( body ) : undefined ),
 		cache   : 'no-cache',
 		headers : {
-			'Content-Type' : 'application/json',
+			...( isFormData ? {} : { 'Content-Type' : 'application/json' } ),
 			Accept         : 'application/json',
 			...headers
 		}
-	} );
+	});
 
 	if ( !response.ok ) {
 		const errorData = await response.json().catch( () => ({}) );
