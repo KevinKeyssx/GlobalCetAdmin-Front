f5141cb95db4bfd204e51899fe08dba6e33e014f
kevinkeyssx <keyss1700@gmail.com>
2026-06-07 23:42:38 -0400

    Params updated

--- src/routes/api/labs/filters/+server.ts
+++ src/routes/api/labs/filters/+server.ts
@@ -1,10 +1,10 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 
-import type { GlobalSearchMobileLab }  from '$lib/types/search';
-import connectRequest, { isApiError } from '$lib/services/fetch.service';
-import { EXTERNAL_ENDPOINTS }         from '$lib/utils/endpoints';
-import { ENV }                        from '$lib/utils/env.server';
-import { mapLabs }                    from '$lib/utils/entityFiles';
+import type { GlobalSearchMobileLab }   from '$lib/types/search';
+import connectRequest, { isApiError }   from '$lib/services/fetch.service';
+import { EXTERNAL_ENDPOINTS }           from '$lib/utils/endpoints';
+import { ENV }                          from '$lib/utils/env.server';
+import { mapLabs }                      from '$lib/utils/entityFiles';
 
 
 interface PaginatedLabsResponse {
@@ -21,31 +21,43 @@ interface PaginatedLabsResponse {
 export const GET: RequestHandler = async ( { url, fetch } ) => {
 	const page       = url.searchParams.get( 'page' ) || '1';
 	const size       = url.searchParams.get( 'size' ) || '10';
+	const query      = url.searchParams.get( 'query' );
+	const active     = url.searchParams.get( 'active' );
 	const categories = url.searchParams.getAll( 'categories' );
 
 	const params = new URLSearchParams( {
 		page,
 		size,
-		includeImages : 'true',
-	} );
+		includeFiles    : 'true',
+		includeKits     : 'true',
+		includeProducts : 'true',
+	});
 
-	categories.forEach( ( id ) => params.append( 'categories', id ) );
+	if ( query ) {
+		params.append( 'query', query );
+	}
 
-	const response = await connectRequest<PaginatedLabsResponse>( {
+	if ( active ) {
+		params.append( 'active', active );
+	}
+
+	categories.forEach(( id ) => params.append( 'categories', id ));
+
+	const response = await connectRequest<PaginatedLabsResponse>({
 		endpoint   : `${ EXTERNAL_ENDPOINTS.LABS.BASE }?${ params.toString() }`,
 		isInternal : false,
 		headers    : {
 			'x-secret' : ENV.INTERNAL_SECRET_KEY,
 		},
 		fetch,
-	} );
+	});
 
-	if ( isApiError( response ) ) {
+	if ( isApiError( response )) {
 		return json( { error : response.message }, { status : response.status || 500 } );
 	}
 
-	return json( {
+	return json({
 		data : mapLabs( response.data || [] ),
 		meta : response.meta,
-	} );
+	});
 };
