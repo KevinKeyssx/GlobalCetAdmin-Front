d22cb20be6a38a4234fe2100d67940d98aa3928e
kevinkeyssx <keyss1700@gmail.com>
2026-06-11 23:00:17 -0400

    FRONTEND_URL unused removed

--- src/lib/utils/env.server.ts
+++ src/lib/utils/env.server.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 import { 
     INTERNAL_SECRET_KEY,
     BACKEND_URL,
-    FRONTEND_URL,
     FILE_MANAGER_URL,
     FILE_MANAGER_FOLDER
 } from '$env/static/private';
@@ -11,18 +10,19 @@ import {
 
 const envSchema = z.object({
     BACKEND_URL         : z.string().min(1),
-    FRONTEND_URL        : z.url().min(1),
+
     INTERNAL_SECRET_KEY : z.string().min(1),
 
-    FILE_MANAGER_URL        : z.string().min(1),
+    FILE_MANAGER_URL    : z.string().min(1),
     FILE_MANAGER_FOLDER : z.string().min(1),
 });
 
 
 const parsedEnv = envSchema.safeParse({
-    INTERNAL_SECRET_KEY,
     BACKEND_URL,
-    FRONTEND_URL,
+
+    INTERNAL_SECRET_KEY,
+
     FILE_MANAGER_URL,
     FILE_MANAGER_FOLDER
 });
@@ -38,10 +38,8 @@ if ( !parsedEnv.success ) {
 
 
 export const ENV = {
-    BACKEND_URL     : parsedEnv.data.BACKEND_URL,
-    FRONTEND_URL    : parsedEnv.data.FRONTEND_URL,
-
-    INTERNAL_SECRET_KEY: parsedEnv.data.INTERNAL_SECRET_KEY,
+    BACKEND_URL         : parsedEnv.data.BACKEND_URL,
+    INTERNAL_SECRET_KEY : parsedEnv.data.INTERNAL_SECRET_KEY,
 
     FILE_MANAGER: {
         URL     : parsedEnv.data.FILE_MANAGER_URL,
