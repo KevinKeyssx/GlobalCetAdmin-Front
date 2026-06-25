70ebce4f4133c994d8fd94e6614001e55ac2ae27
kevinkeyssx <keyss1700@gmail.com>
2026-06-25 01:58:56 -0400

    Page Container added

---
+++ src/lib/components/shared/PageContainer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children?	: Snippet;
+		class?		: string;
+	}
+
+	let {
+		children,
+		class: customClass = 'max-w-6xl space-y-8'
+	} : Props = $props();
+</script>
+
+<main class="relative min-h-[calc(100vh-80px)] px-6 py-6 lg:py-8">
+	<div class="mx-auto { customClass }">
+		{#if children}
+			{@render children()}
+		{/if}
+	</div>
+</main>
