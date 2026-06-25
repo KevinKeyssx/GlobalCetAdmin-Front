603ddc3791c6aebd7e98af80c8222c0a889aee93
kevinkeyssx <keyss1700@gmail.com>
2026-06-25 01:59:45 -0400

    Color styles implemented

--- src/lib/components/shared/Status.svelte
+++ src/lib/components/shared/Status.svelte
@@ -3,21 +3,33 @@
 		status      : boolean;
         textTrue?   : string;
         textFalse?  : string;
+        color?      : boolean;
 	}
 
 	let {
         status,
         textTrue    = 'Activo',
-        textFalse   = 'Inactivo'
+        textFalse   = 'Inactivo',
+        color = false 
     } : Props = $props();
 </script>
 
 {#if status }
-	<span class="rounded-full bg-emerald-500/10 px-2 py-0.5 text-xs text-emerald-400 font-bold border border-emerald-500/20">
+	<span class="rounded-full {
+        color
+            ? 'bg-emerald-500/80 text-emerald-100'
+            : 'bg-emerald-500/10 text-emerald-400'
+        } px-2 py-0.5 text-xs font-bold border border-emerald-500/20"
+    >
         { textTrue }
     </span>
 {:else}
-	<span class="rounded-full bg-red-500/10 px-2 py-0.5 text-xs text-red-400 font-bold border border-red-500/20">
+	<span class="rounded-full {
+        color
+            ? 'bg-red-500/80 text-red-100'
+            : 'bg-red-500/10 text-red-400'
+        } px-2 py-0.5 text-xs font-bold border border-red-500/20"
+    >
         { textFalse }
     </span>
 {/if}
