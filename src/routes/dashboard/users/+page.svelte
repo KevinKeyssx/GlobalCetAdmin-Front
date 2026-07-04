598e935c95825970936b53524f476997ed2fa700
kevinkeyssx <keyss1700@gmail.com>
2026-07-04 19:26:14 -0400

    Users page added

---
+++ src/routes/dashboard/users/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Prisma from "$lib/components/svg/Prisma.svelte";
+</script>
+
+<main class="flex flex-col items-center justify-center pt-52 text-xl text-brand font-semibold">
+    <h2>
+        Próximamente
+    </h2>
+
+    <div class="scale-[2] mt-40 mx-auto flex justify-center animate-wiggle-slow">
+        <Prisma />
+    </div>
+</main>
